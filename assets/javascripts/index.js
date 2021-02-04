3083b118f00cd37e5188d7e1ce067ce7c78352bf
Sam Rystrom <samrystrom@Sams-MacBook-Pro.local>
2021-02-04 15:53:49 -0600

    ls

---
+++ assets/javascripts/index.js
@@ -0,0 +1,60 @@
+function validateForm(form) {
+
+    var data = form.data.value;
+    var retention = form.retention.value;
+    var drive = form.drive.value;
+
+    function validateNumber(num, id) {
+
+        if (num === "" || num < 0 || num === null) {
+        
+            document.getElementById('invalid-' + id).classList.add("visible");
+        
+            return false;
+        
+        }
+        
+        document.getElementById('invalid-' + id).classList.remove("visible");
+        
+        return true;
+    
+    }
+
+    var dataValid = validateNumber(data, "data");
+    var retentionValid = validateNumber(data, "retention");
+    var driveValid = validateNumber(drive, "drive");
+
+    if (dataValid == false || retentionValid == false || driveValid == false) {        
+        return false;
+    }
+
+    return true;
+
+}
+
+function calculate(form) {
+
+    var data = form.data.value;
+    var retention = form.retention.value;
+    var drive = form.drive.value;
+
+    var formValidated = validateForm(form);
+
+    if (formValidated == true) {
+
+        var instances = ((data * 2) * retention) / ((drive * 1024) - 10);
+
+        if (instances <= 2) {instances = 2}
+        else if (instances <= 3) {instances = 3}
+        else if (instances <= 4) {instances = 4}
+        else if (instances <= 10) {instances = 10}
+        else if (instances > 10) {instances = 10};
+
+        // TODO: 20
+
+        document.getElementById("node-count").innerText = instances;
+        document.getElementById("diagram").setAttribute("src", "assets/img/" + instances + ".png");
+
+    }
+
+}
